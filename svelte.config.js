ee6c49fc726d81365b979ffb60b5de8debd654dc
Julius Kreutz <julius@kreutz.dev>
2023-07-07 17:55:26 +1000

    fix: partytown dependency

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,11 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess({
-		preserve: ['partytown']
-	})
+	preprocess: [
+		vitePreprocess({
+			preserve: ['partytown']
+		})
+	]
 };
 
 export default config;
