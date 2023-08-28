5fd2872ff16b14d24ad0a4a6ecda8da0010d3495
yookers <miyookerie@gmail.com>
2023-08-28 14:42:44 -0400

    Added a go back in history button for the database page
    This preserves the snapshot unlike 'goto'.

--- src/routes/database/achievements/[id]/+page.svelte
+++ src/routes/database/achievements/[id]/+page.svelte
@@ -4,9 +4,15 @@
 	import VideoCard from './VideoCard.svelte';
 	import MetadataCard from './MetadataCard.svelte';
 	import type { DatabaseAchievement } from '$types';
+	import { ArrowUpRight } from 'lucide-svelte';
 
 	export let data;
 	const achievementData: DatabaseAchievement = data.achievementData;
+
+    // TODO: Replace with regular 'goto' or 'href' once settings are stored in local storage
+	function handleHistoryBack() {
+		window.history.back();
+	}
 </script>
 
 <svelte:head>
@@ -20,7 +26,19 @@
 
 <main class="flex h-full w-full justify-center px-2 pb-4 text-off_white sm:px-6 sm:pb-6 lg:px-20 xl:px-6">
 	<div class="flex w-full flex-col py-2 md:w-main">
-		<p class="pl-2 text-sm">Database > Achievement > <span class="font-bold">{achievementData.id}</span></p>
+		<p class="pl-2 text-sm">
+			Database / <button on:click={handleHistoryBack}>Achievement</button> / <span class="font-bold">{achievementData.id}</span>
+		</p>
+
+		<button
+			class="group flex items-center pl-2 text-off_white hover:text-galaxy_purple-250"
+			aria-label="Go back to achievement tracker page"
+            on:click={handleHistoryBack}
+		>
+			<p>Return to Tracker</p>
+			<ArrowUpRight class="h-6 w-6 group-hover:-translate-y-0.5" />
+		</button>
+
 		<div class="flex items-center space-x-3 pb-3 pl-2 pt-4 sm:pt-6 md:space-x-4">
 			<!-- Image names are all lowercase, no special characters (including spaces) -->
 			<img
