5845acc3ad5df098a56afa4290ea7a7f2d099e1d
yookers <miyookerie@gmail.com>
2023-09-30 11:04:42 -0400

    Replaced sideways text with vertical due to browser inconsistencies

--- src/routes/[lang=lang]/challenge/15/+page.svelte
+++ src/routes/[lang=lang]/challenge/15/+page.svelte
@@ -16,33 +16,33 @@
 		<div class="relative col-span-1 row-span-1 bg-galaxy_purple-200">
 			<p class="absolute flex w-full justify-center bg-galaxy_purple-700 py-1 text-lg font-bold text-off_white">Patroller</p>
 			<div class="grid h-full grid-cols-5 divide-x font-medium">
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Asta</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Bronya</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Lynx</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Luocha</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Fu Xuan</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Asta</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Bronya</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Lynx</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Luocha</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Fu Xuan</p>
 			</div>
 		</div>
 
 		<div class="relative col-span-1 row-span-1 bg-galaxy_purple-200">
 			<p class="absolute flex w-full justify-center bg-galaxy_purple-700 py-1 text-lg font-bold text-off_white">Encounter</p>
 			<div class="grid h-full grid-cols-5 divide-x font-medium">
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Voidranger: Eliminator</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Voidranger: Trampler</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Incineration Shadewalker</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Decaying Shadow</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Auromaton Direwolf</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Voidranger: Eliminator</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Voidranger: Trampler</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Incineration Shadewalker</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Decaying Shadow</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Auromaton Direwolf</p>
 			</div>
 		</div>
 
 		<div class="relative col-span-1 row-span-1 bg-galaxy_purple-200">
 			<p class="absolute flex w-full justify-center bg-galaxy_purple-700 py-1 text-lg font-bold text-off_white">Location</p>
 			<div class="grid h-full grid-cols-5 divide-x font-medium">
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Herta's Office</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Outlying Snow Plains</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Vagrant Shelter</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Svarog's Base</p>
-				<p class="flex flex-col justify-center pb-2" style="writing-mode: sideways-lr">Orphanage</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Herta's Office</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Outlying Snow Plains</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Vagrant Shelter</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Svarog's Base</p>
+				<p class="flex flex-col items-end justify-center pb-2" style="writing-mode: vertical-lr">Orphanage</p>
 			</div>
 		</div>
 
@@ -50,7 +50,7 @@
 			<div class="flex h-full">
 				<p
 					class="flex justify-center bg-galaxy_purple-700 px-1 text-lg font-bold text-off_white"
-					style="writing-mode: sideways-lr"
+					style="writing-mode: vertical-lr"
 				>
 					Time of Day
 				</p>
@@ -78,7 +78,7 @@
 			<div class="flex h-full">
 				<p
 					class="flex justify-center bg-galaxy_purple-700 px-1 text-lg font-bold text-off_white"
-					style="writing-mode: sideways-lr"
+					style="writing-mode: vertical-lr"
 				>
 					Location
 				</p>
@@ -105,7 +105,7 @@
 			<div class="flex h-full">
 				<p
 					class="flex justify-center bg-galaxy_purple-700 px-1 text-lg font-bold text-off_white"
-					style="writing-mode: sideways-lr"
+					style="writing-mode: vertical-lr"
 				>
 					Encounter
 				</p>
@@ -125,27 +125,15 @@
 	</div>
 
 	<div class="flex w-side flex-col space-y-4 text-off_white">
-        <p class="text-xl font-semibold text-off_white">Clues:</p>
-        {#each [
-            "The 5 people were the person patrolling Svarog's Base, the Afternoon patroller, the one who eliminated the Incineration Shadewalker, Fu Xuan, and the one who patrolled the Vagrant Shelter.",
-            "The Decaying Shadow was eliminated before the Voidranger: Trampler.",
-            "The Decaying Shadow was not found in the Vagrant Shelter.",
-            "The Decaying Shadow was eliminated before Lynx's patrol.",
-            "Of Fu Xuan and Herta's Office patroller, one patrolled at Night, and the other encountered an Automaton Direwolf.",
-            "The one who Patrolled Outlying Snow Plains was Fu Xuan.",
-            "Asta patrolled after Luocha.",
-            "The Voidranger: Trampler was not eliminated by Lynx.",
-            "Svarog's Base was not patrolled by Luocha.",
-            "Either the Morning patroller or the Night patroller patrolled the Orphanage.",
-            "The Automaton Direwolf was eliminated by Asta."
-          ] as section, index}
-            <div class="flex items-start mb-2">
-              <button 
-                class="shrink-0 border-2 border-galaxy_purple-200 w-6 h-6 mr-3 mt-1.5 {clues[index] ? 'bg-galaxy_purple-200' : ''}" 
-                on:click={() => toggleClueComplete(index)}
-              ></button>
-              <p>{section}</p>
-            </div>
-          {/each}
+		<p class="text-xl font-semibold text-off_white">Clues:</p>
+		{#each ["The 5 people were the person patrolling Svarog's Base, the Afternoon patroller, the one who eliminated the Incineration Shadewalker, Fu Xuan, and the one who patrolled the Vagrant Shelter.", 'The Decaying Shadow was eliminated before the Voidranger: Trampler.', 'The Decaying Shadow was not found in the Vagrant Shelter.', "The Decaying Shadow was eliminated before Lynx's patrol.", "Of Fu Xuan and Herta's Office patroller, one patrolled at Night, and the other encountered an Automaton Direwolf.", 'The one who Patrolled Outlying Snow Plains was Fu Xuan.', 'Asta patrolled after Luocha.', 'The Voidranger: Trampler was not eliminated by Lynx.', "Svarog's Base was not patrolled by Luocha.", 'Either the Morning patroller or the Night patroller patrolled the Orphanage.', 'The Automaton Direwolf was eliminated by Asta.'] as section, index}
+			<div class="mb-2 flex items-start">
+				<button
+					class="mr-3 mt-1.5 h-6 w-6 shrink-0 border-2 border-galaxy_purple-200 {clues[index] ? 'bg-galaxy_purple-200' : ''}"
+					on:click={() => toggleClueComplete(index)}
+				></button>
+				<p>{section}</p>
+			</div>
+		{/each}
 	</div>
 </div>
