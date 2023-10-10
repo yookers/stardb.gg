64151f216706b59ecf37a8f326ef6f43fe2c309a
yookers <miyookerie@gmail.com>
2023-10-10 15:49:44 -0400

    Maybe this might fix the adblocker 404 errors?

--- src/components/BannerAd.svelte
+++ src/components/BannerAd.svelte
@@ -1,42 +1,44 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-    
-	onMount(() => {
-		window['nitroAds'].createAd('stardb-banner', {
-			refreshLimit: 20,
-			refreshTime: 30,
-			renderVisibleOnly: false,
-			refreshVisibleOnly: true,
-			sizes: [['728', '90']],
-			report: {
-				enabled: true,
-				icon: true,
-				wording: 'Report Ad',
-				position: 'top-right'
-			},
-			//demo: true
-		});
 
-		window['nitroAds'].createAd('stardb-mobile-banner', {
-			refreshLimit: 20,
-			refreshTime: 30,
-			renderVisibleOnly: false,
-			refreshVisibleOnly: true,
-			sizes: [
-				['320', '50'],
-				['300', '50'],
-			],
-			report: {
-				enabled: true,
-				icon: true,
-				wording: 'Report Ad',
-				position: 'top-right'
-			},
-			//demo: true
-		});
+	onMount(() => {
+		if (window['nitroAds']) {
+			window['nitroAds'].createAd('stardb-banner', {
+				refreshLimit: 20,
+				refreshTime: 30,
+				renderVisibleOnly: false,
+				refreshVisibleOnly: true,
+				sizes: [['728', '90']],
+				report: {
+					enabled: true,
+					icon: true,
+					wording: 'Report Ad',
+					position: 'top-right'
+				}
+				//demo: true
+			});
+
+			window['nitroAds'].createAd('stardb-mobile-banner', {
+				refreshLimit: 20,
+				refreshTime: 30,
+				renderVisibleOnly: false,
+				refreshVisibleOnly: true,
+				sizes: [
+					['320', '50'],
+					['300', '50']
+				],
+				report: {
+					enabled: true,
+					icon: true,
+					wording: 'Report Ad',
+					position: 'top-right'
+				}
+				//demo: true
+			});
+		}
 	});
 </script>
 
-<div id="stardb-banner" class="hidden py-2 px-2 md:block"></div>
+<div id="stardb-banner" class="hidden px-2 py-2 md:block"></div>
 
-<div id="stardb-mobile-banner" class="py-2 px-2 md:hidden"></div>
+<div id="stardb-mobile-banner" class="px-2 py-2 md:hidden"></div>
