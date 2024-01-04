faff0fa2e66fcde44169e2982af6d9db131a61c7
yookers <miyookerie@gmail.com>
2024-01-04 18:54:19 -0500

    Added warp tracker import instruction page

---
+++ src/routes/[lang=lang]/warp-tracker/+page.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import InstructionCard from './InstructionCard.svelte';
+	import PopUpMessage from '$components/PopUpMessage.svelte';
+	import { MessageType } from '$types';
+	import { goto } from '$app/navigation';
+	import { locale } from '$store';
+
+	const apiURL = import.meta.env.VITE_PUBLIC_SERVER_API_URL;
+	let warpURL: string;
+	let importStatus: 'pending' | 'running' | 'finished' | 'failed';
+	let gachaType: 'standard' | 'departure' | 'special' | 'lc';
+	let warpCount: number;
+	let pollingInterval: NodeJS.Timeout;
+
+	async function postWarpsImport(url: string) {
+		try {
+			const response = await fetch(`${apiURL}/warps-import`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ url })
+			});
+			if (response.ok) {
+				const data = await response.json();
+				startPolling(data.uid);
+			} else {
+				console.error('Failed to start import:', response.status);
+			}
+		} catch (error) {
+			console.error('Error posting warps import:', error);
+		}
+	}
+
+	function startPolling(uid: number) {
+		pollingInterval = setInterval(async () => {
+			try {
+				const response = await fetch(`${apiURL}/warps-import/${uid}`);
+				const data = await response.json();
+
+				if (response.ok) {
+					importStatus = data.status;
+					gachaType = data.gacha_type;
+					warpCount = data[gachaType];
+
+					if (importStatus === 'finished') {
+						clearInterval(pollingInterval);
+						goto(`/${$locale}/warp-viewer/${uid}`);
+						// Handle the finished state, e.g., display results
+					}
+				} else {
+					console.error('Failed to get import status:', response.status);
+				}
+			} catch (error) {
+				console.error('Error polling warps import:', error);
+			}
+		}, 1000); // Poll every 1 second
+	}
+</script>
+
+<svelte:head>
+	<title></title>
+	<meta name="description" content="" />
+	<link rel="canonical" href="" />
+</svelte:head>
+
+<main class="flex h-full w-full justify-center px-2 pb-4 text-off_white sm:px-6 sm:pb-6 lg:px-20 xl:px-6">
+	<div class="flex w-full flex-col py-2 md:w-main">
+		<div class="flex items-center space-x-3 pb-3 pl-2 pt-4 sm:pt-6 md:space-x-4">
+			<div class="">
+				<h1 class="text-xl font-bold md:text-3xl">Warp Tracker</h1>
+				<h2 class="text-base text-galaxy_purple-150 md:text-lg">Import your warps here.</h2>
+			</div>
+		</div>
+		<div class="flex flex-col space-y-3 sm:space-y-4">
+			<InstructionCard />
+
+			<div class="flex gap-x-3">
+				<input class="h-8 w-full bg-off_white text-galaxy_purple-700" type="text" required bind:value={warpURL} />
+				<button
+					class="text-white w-8 rounded-xl bg-galaxy_purple-650 px-4 py-2 text-sm font-bold hover:bg-galaxy_purple-600"
+					on:click={() => postWarpsImport(warpURL)}
+				></button>
+			</div>
+		</div>
+	</div>
+</main>
+
+{#if importStatus === 'pending'}
+	<PopUpMessage
+		messageType={MessageType.LOADING}
+		messageContent="Currently importing {warpCount} {gachaType} warps... Please wait."
+	/>
+{:else if importStatus === 'finished'}
+	<PopUpMessage messageType={MessageType.SUCCESS} messageContent="Successfully imported warps!" />
+{:else if importStatus === 'failed'}
+	<PopUpMessage messageType={MessageType.ERROR} messageContent="Failed to import warps. Please try again." />
+{/if}
---
+++ src/routes/[lang=lang]/warp-tracker/InstructionCard.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { Pencil, ArrowUpRight, Clipboard } from 'lucide-svelte';
+	import BaseCard from '$components/BaseCard.svelte';
+
+	let showCard = true;
+
+	async function copyToClipboard(text: string) {
+		try {
+			await navigator.clipboard.writeText(text);
+		} catch (err) {
+			console.error('Failed to copy: ', err);
+		}
+	}
+</script>
+
+<BaseCard label="Instructions" title="Instructions">
+	<div slot="icon">
+		<Pencil class="h-5 w-5 text-off_white" />
+	</div>
+	<div slot="content" class="pb-3 pt-4">
+		<div class="flex flex-col space-y-3 text-sm sm:text-base">
+			<div class="space-y-3 px-6 pb-3">
+				<ol class="space-y-3">
+					<li>
+						<span class="font-bold">Step 1:</span> Verify that your UID is listed on the StarDB Leaderboard.
+					</li>
+					<li>
+						<span class="font-bold">Step 2:</span> Start Honkai: Star Rail on your PC and navigate to your warp records.
+					</li>
+					<li>
+						<span class="font-bold">Step 3:</span> Open Windows PowerShell. You can do this by searching for 'PowerShell' in the
+						Windows search bar.
+					</li>
+					<li>
+						<span class="font-bold">Step 4:</span> Copy and paste the following command into PowerShell and press enter:
+						<div class="space-y-2">
+							<p>For Global Server players, copy and execute the following command:</p>
+							<div class="flex justify-between w-80 rounded-sm bg-galaxy_purple-250 py-2 pl-3 font-mono text-sm text-galaxy_purple-800">
+								<p>iwr -useb stardb.gg/warp | iex</p>
+								<button class="mr-3" on:click={() => copyToClipboard('iwr -useb stardb.gg/warp | iex')}>
+									<Clipboard class="h-5 w-5"></Clipboard>
+								</button>
+							</div>
+
+                            <p>For CN Server players, copy and execute the following command:</p>
+                            <div class="flex justify-between w-80 rounded-sm bg-galaxy_purple-250 py-2 pl-3 font-mono text-sm text-galaxy_purple-800">
+								<p>iwr -useb stardb.gg/warp_cn | iex</p>
+								<button class="mr-3" on:click={() => copyToClipboard('iwr -useb stardb.gg/warp_cn | iex')}>
+									<Clipboard class="h-5 w-5"></Clipboard>
+								</button>
+							</div>
+						</div>
+					</li>
+                    <li>
+                        <span class="font-bold">Step 5:</span> Paste the output below. It may take a few minutes for your warps to be imported.
+                    </li>
+				</ol>
+			</div>
+		</div>
+	</div>
+</BaseCard>
