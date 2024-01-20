2f2bd2f67b1d5717c0862baa5b601a36d64a590f
Julius Kreutz <julius@kreutz.dev>
2024-01-20 19:31:01 +1100

    Oops

--- src/routes/[lang=lang]/+page.svelte
+++ src/routes/[lang=lang]/+page.svelte
@@ -27,9 +27,6 @@
 			searchPlayer(searchQuery);
 		}
 	}
-
-	import { Youtube } from 'lucide-svelte';
-	import BaseCard from '$components/BaseCard.svelte';
 </script>
 
 <svelte:head>
@@ -61,25 +58,6 @@
 
 	<link rel="canonical" href="https://stardb.gg" />
 </svelte:head>
-
-<BaseCard label="Video guide" title="Video Guide">
-	<div slot="icon">
-		<Youtube class="h-5 w-5 text-off_white" />
-	</div>
-	<div slot="content" class="p-1.5 sm:p-3">
-		<!-- (pb-[56.25%]) 16:9 Aspect ratio  -->
-		<div class="relative flex w-full pb-[56.25%]">
-			<iframe
-				class="absolute left-0 h-full w-full rounded-xl border-2 border-galaxy_purple-650"
-				src="https://www.youtube-nocookie.com/embed/J3d0GT4mBo8?rel=0&enablejsapi=1"
-				title="Video guide on how to obtain the achievement"
-				frameborder="0"
-				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-				allowfullscreen
-			></iframe>
-		</div>
-	</div>
-</BaseCard>
 
 <main class="relative flex h-full flex-col items-center justify-center px-4 text-off_white lg:px-20">
 	<a href="https://ko-fi.com/stardb" aria-label="Ko-fi link" target="_blank" rel="noopener noreferrer">
