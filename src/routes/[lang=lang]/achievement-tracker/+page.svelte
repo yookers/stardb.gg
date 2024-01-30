98737f52821ea83aa566e44cb8df2ce9776fea4b
Julius Kreutz <julius@kreutz.dev>
2024-01-31 01:51:52 +1100

    FIX

--- src/routes/[lang=lang]/achievement-tracker/+page.svelte
+++ src/routes/[lang=lang]/achievement-tracker/+page.svelte
@@ -41,8 +41,8 @@
 		showIncomplete = JSON.parse(localStorage.getItem('showIncomplete') || 'true');
 		selectedSeries = JSON.parse(localStorage.getItem('selectedSeries') || '"Show All"');
 		selectedVersions = new Set(JSON.parse(localStorage.getItem('selectedVersions') || '[]'));
-		searchQuery = JSON.parse(localStorage.getItem('searchQuery') || '\"\"');
-		selectedDifficulty = JSON.parse(localStorage.getItem('selectedDifficulty') || 'all');
+		searchQuery = JSON.parse(localStorage.getItem('searchQuery') || '""');
+		selectedDifficulty = JSON.parse(localStorage.getItem('selectedDifficulty') || '"all"');
 		showHidden = JSON.parse(localStorage.getItem('showHidden') || 'true');
 		sortOrder = JSON.parse(localStorage.getItem('sortOrder') || '"default"');
 	});
