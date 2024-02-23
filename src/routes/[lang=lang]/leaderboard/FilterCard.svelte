33caaa9bbd8d79f4e07c37a3ba60f618ef4ce700
Julius Kreutz <julius@kreutz.dev>
2024-02-23 17:11:23 +1100

    moooore

--- src/routes/[lang=lang]/leaderboard/FilterCard.svelte
+++ src/routes/[lang=lang]/leaderboard/FilterCard.svelte
@@ -58,7 +58,7 @@
 		jp: 'アメリカ',
 		kr: '북미',
 		pt: 'NA',
-		ru: 'Северная Америка',
+		ru: 'Америка',
 		th: 'NA',
 		vi: 'America'
 	} as any;
@@ -110,6 +110,38 @@
 		th: 'CN',
 		vi: 'Trung Quốc'
 	} as any;
+
+	const worldText = {
+		chs: '全球排名',
+		cht: '全球排名',
+		de: 'Welt',
+		en: 'World',
+		es: 'Mundo',
+		fr: 'Monde',
+		id: 'Dunia',
+		jp: '世界',
+		kr: '세계',
+		pt: 'Global',
+		ru: 'Мир',
+		th: 'โลก',
+		vi: 'Thế giới'
+	} as any;
+
+	const regionText = {
+		chs: '地区排名',
+		cht: '詞服器排名',
+		de: 'Region',
+		en: 'Region',
+		es: 'Región',
+		fr: 'Région',
+		id: 'Wilayah',
+		jp: '地域',
+		kr: '지역',
+		pt: 'Região',
+		ru: 'Регион',
+		th: 'ภูมิภาค',
+		vi: 'Khu vực'
+	} as any;
 </script>
 
 <BaseCard label={filterText[$locale]} title={filterText[$locale]}>
@@ -123,7 +155,7 @@
 		>
 			<div class="flex flex-wrap gap-2 gap-y-3">
 				<button
-					class="flex h-10 w-16 flex-shrink-0 items-center justify-center rounded-full border-2 border-galaxy_purple-400 font-extrabold text-galaxy_purple-400 hover:bg-galaxy_purple-400 hover:text-space_dark md:w-20"
+					class="flex h-10 w-24 flex-shrink-0 items-center justify-center rounded-full border-2 border-galaxy_purple-400 font-extrabold text-galaxy_purple-400 hover:bg-galaxy_purple-400 hover:text-space_dark"
 					class:bg-galaxy_purple-400={regionFilter === 'all'}
 					class:bg-galaxy_purple-750={regionFilter !== 'all'}
 					class:text-space_dark={regionFilter === 'all'}
@@ -133,7 +165,7 @@
 					<p>{allText[$locale]}</p>
 				</button>
 				<button
-					class="flex h-10 w-16 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_yellow bg-galaxy_purple-750 font-extrabold text-neon_yellow hover:bg-neon_yellow hover:text-space_dark md:w-20"
+					class="flex h-10 w-24 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_yellow bg-galaxy_purple-750 font-extrabold text-neon_yellow hover:bg-neon_yellow hover:text-space_dark"
 					class:bg-neon_yellow={regionFilter === 'na'}
 					class:text-space_dark={regionFilter === 'na'}
 					aria-label="Filter by NA region"
@@ -142,7 +174,7 @@
 					<p>{naText[$locale]}</p>
 				</button>
 				<button
-					class="flex h-10 w-16 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_pink bg-galaxy_purple-750 font-extrabold text-neon_pink hover:bg-neon_pink hover:text-space_dark md:w-20"
+					class="flex h-10 w-24 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_pink bg-galaxy_purple-750 font-extrabold text-neon_pink hover:bg-neon_pink hover:text-space_dark"
 					class:bg-neon_pink={regionFilter === 'eu'}
 					class:text-space_dark={regionFilter === 'eu'}
 					aria-label="Filter by EU region"
@@ -151,7 +183,7 @@
 					<p>{euText[$locale]}</p>
 				</button>
 				<button
-					class="flex h-10 w-16 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_green bg-galaxy_purple-750 font-extrabold text-neon_green hover:bg-neon_green hover:text-space_dark md:w-20"
+					class="flex h-10 w-24 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_green bg-galaxy_purple-750 font-extrabold text-neon_green hover:bg-neon_green hover:text-space_dark"
 					class:bg-neon_green={regionFilter === 'asia'}
 					class:text-space_dark={regionFilter === 'asia'}
 					aria-label="Filter by ASIA region"
@@ -160,7 +192,7 @@
 					<p>{asiaText[$locale]}</p>
 				</button>
 				<button
-					class="flex h-10 w-16 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_blue bg-galaxy_purple-750 font-extrabold text-neon_blue hover:bg-neon_blue hover:text-space_dark md:w-20"
+					class="flex h-10 w-24 flex-shrink-0 items-center justify-center rounded-full border-2 border-neon_blue bg-galaxy_purple-750 font-extrabold text-neon_blue hover:bg-neon_blue hover:text-space_dark"
 					class:bg-neon_blue={regionFilter === 'cn'}
 					class:text-space_dark={regionFilter === 'cn'}
 					aria-label="Filter by CN region"
@@ -173,10 +205,10 @@
 				class="relative flex h-10 w-40 select-none items-center rounded-full bg-galaxy_purple-200 text-sm font-bold text-galaxy_purple-700"
 			>
 				<button class="h-full w-20 px-4" aria-label="Set search type to World" on:click={() => setRanking('World')}
-					>World</button
+					>{worldText[$locale]}</button
 				>
 				<button class="h-full w-20 px-4" aria-label="Set search type to Region" on:click={() => setRanking('Region')}
-					>Region</button
+					>{regionText[$locale]}</button
 				>
 				<span
 					class="absolute flex h-8 w-20 items-center justify-center rounded-full bg-galaxy_purple-700 text-galaxy_purple-200 duration-300
