0519fbc988b022541ef36f47f3291b0412da93dc
Julius Kreutz <julius@kreutz.dev>
2024-04-18 10:50:00 +1000

    Working on deprecated project

--- src/routes/[lang=lang]/zzz/achievement-tracker/+page.svelte
+++ src/routes/[lang=lang]/zzz/achievement-tracker/+page.svelte
@@ -12,7 +12,7 @@
 		property="og:description"
 		content="The best achievement tracker for Zenless Zone Zero. Includes guides, videos, and cultural references powered by the completionist community."
 	/>
-	<meta property="og:image" content="https://stardb.gg/images/StarDB.GG_Featured_Image.webp" />
+	<meta property="og:image" content="https://stardb.gg/images/achievement_tracker.jpg" />
 	<meta property="og:image:width" content="1200" />
 	<meta property="og:image:height" content="630" />
 	<!-- Twitter -->
@@ -23,11 +23,31 @@
 		name="twitter:description"
 		content="The best achievement tracker for Zenless Zone Zero. Includes guides, videos, and cultural references powered by the completionist community."
 	/>
-	<meta name="twitter:image" content="https://stardb.gg/images/StarDB.GG_Featured_Image.webp" />
+	<meta name="twitter:image" content="https://stardb.gg/images/achievement_tracker.jpg" />
 
 	<link rel="canonical" href="https://stardb.gg/zzz/achievement-tracker" />
 </svelte:head>
 
-<div class="flex h-full items-center justify-center space-x-8 space-y-8 text-off_white md:flex-row md:space-y-0">
-	<p class="text-9xl font-bold">WIP</p>
+<div class="mx-8 flex h-full flex-col items-center justify-center space-y-4 text-off_white">
+	<h1 class="text-3xl font-bold">Zenless Zone Zero Achievement Tracker tool by stardb.gg.</h1>
+
+	<p>
+		During CBT2, the game featured 60 achievements from arcade games, which rewarded players with <strong>Film</strong>.
+		<br />
+		Film is the key currency for in-game pulls, comparable to Genshin's Primogems or Honkai: Star Rail's Stellar Jades.
+	</p>
+
+	<p>
+		The <strong>ZZZ Achievement Tracker tool</strong> is a work-in-progress and is slated for release as the game approaches its
+		global launch.
+		<br />
+		The tracker will be complete with text and video guides, cultural references, and powerful filters to help you hunt achievements
+		efficiently.
+	</p>
+
+	<p>
+		For realtime updates join our discord server here -> <a href="https://discord.gg/chives" target="_blank"
+			>https://discord.gg/chives</a
+		>
+	</p>
 </div>
---
+++ src/routes/[lang=lang]/zzz/signal-tracker/+page.svelte
@@ -0,0 +1,128 @@
+<svelte:head>
+	<title>Signal Tracker for Zenless Zone Zero | stardb.gg</title>
+	<meta
+		name="description"
+		content="The best signal tracker for Zenless Zone Zero. Import your signal history and view your pity counter."
+	/>
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://stardb.gg/zzz/signal-tracker" />
+	<meta property="og:title" content="Signal Tracker for Zenless Zone Zero | stardb.gg" />
+	<meta
+		property="og:description"
+		content="The best signal tracker for Zenless Zone Zero. Import your signal history and view your pity counter."
+	/>
+	<meta property="og:image" content="https://stardb.gg/images/ZZZ_Signal_Tracker.jpg" />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:url" content="https://stardb.gg/zzz/signal-tracker" />
+	<meta name="twitter:title" content="Signal Tracker for Zenless Zone Zero | stardb.gg" />
+	<meta
+		name="twitter:description"
+		content="The best signal tracker for Zenless Zone Zero. Import your signal history and view your pity counter."
+	/>
+	<meta name="twitter:image" content="https://stardb.gg/images/ZZZ_Signal_Tracker.jpg" />
+
+	<link rel="canonical" href="https://stardb.gg/zzz/signal-tracker" />
+</svelte:head>
+
+<div class="m-8 flex h-full flex-col items-center justify-center space-y-4 text-off_white">
+	<h1 class="text-3xl font-bold">Zenless Zone Zero Signal Tracker</h1>
+	<p>
+		<strong>Zenless Zone Zero (ZZZ)</strong>, the latest ARPG from Hoyoverse, features a gacha system similar to other titles
+		from the developer but introduces unique elements and terminologies specific to ZZZ.
+		<br />
+		This summary is based on insights from the game&#39;s second Closed Beta Test (CBT2).
+	</p>
+	<blockquote>
+		<p>
+			<strong
+				>The team at stardb.gg is working tirelessly on creating a Zenless Zone Zero ZZZ Signal Tracker, and will have something
+				prepared closer to the global launch of the game.</strong
+			>
+		</p>
+		<p>
+			For realtime updates join our discord server here -> <a href="https://discord.gg/chives" target="_blank"
+				>https://discord.gg/chives</a
+			>
+		</p>
+	</blockquote>
+	<h2 class="text-2xl font-bold">Gacha System and Character Rarities in Zenless Zone Zero</h2>
+	<ol class="list-decimal">
+		<li>
+			<p><strong>Main Summonable Items</strong>:</p>
+			<ul class="ml-4 list-disc">
+				<li>
+					<strong>Agents</strong>: Playable characters that expand your team&#39;s capabilities. Duplicate agents enhance
+					existing characters by granting them new abilities.
+				</li>
+				<li>
+					<strong>W-Engines</strong>: These are akin to weapons or Light Cones in Honkai: Star Rail, that equip agents with
+					enhanced stats or special abilities. Notably, S-Rank W-Engines are not available from the gacha pulls as per CBT2.
+				</li>
+				<li>
+					<strong>Bangboos</strong>: Mascot-like creatures that accompany agents in battle, providing team-wide boosts to ATK,
+					HP, DEF, and Impact. Available in A-Rank and B-Rank variants through summons.
+				</li>
+			</ul>
+		</li>
+		<li>
+			<p><strong>Banners (Channels)</strong>:</p>
+			<ul class="ml-4 list-disc">
+				<li>
+					<strong>Exclusive Channel</strong>: This is for limited-time S-Rank characters, similar to the system in other
+					Hoyoverse games. If an S-Rank pull does not yield the rate-up unit, the next S-Rank is guaranteed to be the featured
+					character.
+				</li>
+				<li>
+					<strong>Standard Channel</strong>: Features a fixed roster of S-Rank units without specific rate-ups. Offers a
+					beginner-friendly discount on initial pulls, reducing the cost and guaranteeing an S-Rank agent within the first 50
+					pulls.
+				</li>
+			</ul>
+		</li>
+		<li>
+			<p><strong>ZZZ Pity System</strong>:</p>
+			<ul class="ml-4 list-disc">
+				<li>
+					The gacha ensures a minimum frequency of high-rarity pulls. Players are guaranteed an A-Rank item every ten pulls and
+					an S-Rank agent every 90 pulls, with increased odds starting at 75 and ending at 90.
+				</li>
+			</ul>
+		</li>
+	</ol>
+	<h2 class="text-2xl font-bold">Gacha Rates and Expectations</h2>
+	<ul class="list-disc">
+		<li><strong>S-Rank Signal (Agents Only)</strong>: 0.6%</li>
+		<li><strong>A-Rank Signal</strong>: 7.2% (divided among agents, W-Engines, and Bangboos)</li>
+		<li><strong>B-Rank Signal</strong>: 92.0% (also divided among the three categories)</li>
+	</ul>
+	<h2 class="text-2xl font-bold">Character Rarities and Availability</h2>
+	<ul class="list-disc">
+		<li>
+			<strong>S-Rank Agents</strong>: The pinnacle of rarity, these agents are available either from the Exclusive Channel
+			during their limited availability or from the Standard Channel.
+		</li>
+		<li>
+			<strong>A-Rank Agents</strong>: More commonly pulled characters, available from both Exclusive and Standard Channels, with
+			specific agents featured more frequently on rate-up during exclusive banner periods.
+		</li>
+	</ul>
+	<h2 class="text-2xl font-bold">Additional Details from CBT2</h2>
+	<ul class="list-disc">
+		<li>
+			<strong>Limited Channels for W-Engines and Bangboos</strong>: As of CBT2, it remains unclear whether separate channels for
+			these items will be introduced, as they are primarily earned through gameplay rather than gacha pulls.
+		</li>
+	</ul>
+	<h2 class="text-2xl font-bold">Signal Tracker in Zenless Zone Zero</h2>
+	<p>
+		The <strong>Signal Tracker</strong> is an awesome tool to help players monitor their pity counter and optimize banner pulls
+		strategically.
+		<br />
+		Mihoyo has the tendency to delete pull history every 6 months, so back up your data. Save your pull history by importing all
+		your signal searches into stardb.gg&#39;s ZZZ Signal Tracker.
+	</p>
+</div>
