29b6b6162346a4d212d8381e7f3a1094ce366d23
Julius Kreutz <julius@kreutz.dev>
2024-05-15 06:39:46 +1000

    New import

--- src/routes/[lang=lang]/import/+page.svelte
+++ src/routes/[lang=lang]/import/+page.svelte
@@ -3,32 +3,26 @@
 	import { MessageType } from '$types';
 
 	const apiURL = import.meta.env.VITE_PUBLIC_SERVER_API_URL;
-	let fileInput: HTMLInputElement;
+	let input = '';
 	let scannerStatus: '' | 'finished' | 'failed';
 
 	async function postScannerImport() {
-		// Request body is a file.
-		if (!fileInput) {
-			return;
-		}
-		const formData = new FormData();
-		formData.append('file', fileInput.files[0]);
-
 		try {
 			const response = await fetch(`${apiURL}/users/me/import`, {
 				method: 'PUT',
-				body: formData
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: input
 			});
 
 			if (response.ok) {
 				scannerStatus = 'finished';
 			} else {
 				scannerStatus = 'failed';
-				console.error('Failed to upload file:', response.status);
 			}
 		} catch (error) {
 			scannerStatus = 'failed';
-			console.error('Error uploading file:', error);
 		}
 	}
 </script>
@@ -37,16 +31,20 @@
 	<div class="flex w-full flex-col py-2 md:w-main">
 		<div class="flex items-center space-x-3 pb-3 pl-2 pt-4 sm:pt-6 md:space-x-4">
 			<div class="">
-				<h1 class="text-xl font-bold md:text-3xl">Achievement Import</h1>
-				<h2 class="text-base text-galaxy_purple-150 md:text-lg">Import your achievements here.</h2>
+				<h1 class="text-xl font-bold md:text-3xl">Import</h1>
+				<h2 class="text-base text-galaxy_purple-150 md:text-lg">Import here.</h2>
 			</div>
 		</div>
 
 		<div class="flex flex-col space-y-2">
-			<span class="pt-8">Import from Achievement Scanner (JSON)</span>
-            <p class="text-sm text-neon_pink">Warning: Importing from scanner will overwrite your current saved achievements.</p>
-			<div class="flex gap-x-3">
-				<input type="file" bind:this={fileInput} accept=".json" />
+			<span class="pt-8"
+				>Import from <a href="https://github.com/juliuskreutz/stardb-exporter" target="_blank">Stardb Exporter</a></span
+			>
+			<p class="text-sm text-neon_pink">
+				Warning: Importing from exporter will overwrite your current saved achievements and books.
+			</p>
+			<div class="flex gap-x-3 text-space_dark">
+				<input type="text" bind:value={input} />
 				<button
 					class="text-white h-fit w-fit rounded-xl bg-galaxy_purple-650 px-4 py-2 text-sm font-bold hover:bg-galaxy_purple-600"
 					on:click={postScannerImport}>Import</button
@@ -57,7 +55,7 @@
 </main>
 
 {#if scannerStatus === 'finished'}
-	<PopUpMessage messageType={MessageType.SUCCESS} messageContent="Successfully uploaded file!" />
+	<PopUpMessage messageType={MessageType.SUCCESS} messageContent="Successfully imported!" />
 {:else if scannerStatus === 'failed'}
-	<PopUpMessage messageType={MessageType.FAIL} messageContent="Failed to upload file." />
+	<PopUpMessage messageType={MessageType.FAIL} messageContent="Failed to import." />
 {/if}
